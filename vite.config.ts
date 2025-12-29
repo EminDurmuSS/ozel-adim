b4215d2219d185ea31ce796a0cb47b801b23ac4b
Emin durmuş <emin.durmuss80@gmail.com>
2025-12-29 12:57:29 +0300

    Fix build: switch from terser to esbuild minifier and simplify aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@': '/src',
+      '@components': '/src/components',
+      '@assets': '/src/assets',
+      '@styles': '/src/styles',
+      '@utils': '/src/utils',
+      '@hooks': '/src/hooks',
     },
   },
   build: {
@@ -24,12 +23,7 @@ export default defineConfig({
         },
       },
     },
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
+    minify: 'esbuild',
   },
   server: {
     port: 3000,
